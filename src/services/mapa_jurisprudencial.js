93e0ae9f39a4aab604f6599b8e75c2ab7e4f495f
claudiainbytes <claudiaeforero@hotmail.com>
2025-01-17 15:08:16 -0500

    mapa_jurisprudencial_creacion_servicio_datagraph

--- src/services/mapa_jurisprudencial.js
+++ src/services/mapa_jurisprudencial.js
@@ -56,7 +56,6 @@ const getMapaDptos = () => {
 }
 
 const getDepartamentos = () => {
-
   const config = {
     headers: {
       'Authorization': `Bearer ${process.env.REACT_APP_API_ACCESS_TOKEN}`,
@@ -77,4 +76,25 @@ const getDepartamentos = () => {
   });
 };
 
-export default { getProvidencias, getMapaDptos, getDepartamentos };
+const getDetailsGraph = (paramsURL) => {
+  const config = {
+    headers: {
+      'Authorization': `Bearer ${process.env.REACT_APP_API_ACCESS_TOKEN}`,
+      'user': process.env.REACT_APP_API_USER,
+      'password': process.env.REACT_APP_API_PASS
+    },
+    params: { }
+  };
+  const request =  axios.get(`https://relatoria.jep.gov.co/detailsgraph?${paramsURL}`, config);
+  return request.then(response => { 
+    if((response.data.status !== undefined) || (response.data.status === 401) || (response.data.status === 403)) {
+      return { "data": [], "status_info": { "status": response.data.status, "reason": response.data.reason }};
+    } else {
+      return { "data": response.data[0].totales, "status_info": { "status": 200, "reason": "OK" } };
+    }
+  }).catch(error => { 
+    return { "data": [], "status_info": { "status": error.response.data.status, "reason": error.response.data.reason } };
+  });
+};
+
+export default { getProvidencias, getMapaDptos, getDepartamentos, getDetailsGraph };
