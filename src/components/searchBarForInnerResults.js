cb1aff61bd9e8cf6704f9784c4036166298aaf4c
claudiainbytes <claudiaeforero@hotmail.com>
2025-03-04 15:05:05 -0500

    searchBarForInnerResults_boton_reestablecer_resultados

--- src/components/searchBarForInnerResults.js
+++ src/components/searchBarForInnerResults.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useRef } from 'react';
-import { Button, TextField, Stack, Autocomplete } from '@mui/material';
+import { Button, TextField, Stack, Autocomplete, Box } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import '../App.css';
 
@@ -16,27 +16,32 @@ const SearchBarForInnerResults = ({handlerInnerSearch, ref = null }) => {
   };
     
   return (
-    <Stack className='autocomplete_bar_search_small'>
-      <Autocomplete
-        id="searchBarForInnerResults"
-        value={valueSearchBar}
-        freeSolo
-        options={options.map((option) => option.title)}
-        renderInput={(params) => <TextField {...params}
-                                    id="searchBarForInnerResultsTextField"
-                                    ref={inputRef}
-                                    label="Buscar en los resultados" 
-                                    placeholder=""
-                                    inputProps={{
-                                          ...params.inputProps,
-                                          maxLength: 80
-                                    }}
-                                    onChange={updateSelectedValue}
-                                  />
-        }
-      />
-      <Button className="autocomplete_button_small button_primary z-index_front" onClick={()=>handlerInnerSearch(valueSearchBar)} ><SearchIcon /></Button>
-    </Stack>
+    <div className="autocomplete_bar_inner_search">
+      <Stack className='autocomplete_bar_search_small'>
+        <Autocomplete
+          id="searchBarForInnerResults"
+          value={valueSearchBar}
+          freeSolo
+          options={options.map((option) => option.title)}
+          renderInput={(params) => <TextField {...params}
+                                      id="searchBarForInnerResultsTextField"
+                                      ref={inputRef}
+                                      label="Buscar en los resultados" 
+                                      placeholder=""
+                                      inputProps={{
+                                            ...params.inputProps,
+                                            maxLength: 80
+                                      }}
+                                      onChange={updateSelectedValue}
+                                    />
+          }
+        />
+        <Button className="autocomplete_button_small button_primary z-index_front" onClick={()=>handlerInnerSearch(valueSearchBar)} ><SearchIcon /></Button>
+      </Stack>
+      <Box>
+        <Button variant="outlined" className='autocomplete_bar_inner_search_undo_results' size="small" onClick={() => {/* Add your undo search logic here */}}>Reestablecer resultados</Button>
+      </Box>
+    </div>
   ); 
 }
 
