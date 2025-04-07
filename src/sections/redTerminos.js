55a1c9b2c1fb40dbf8d46db0c9af98299244f8ee
claudiainbytes <claudiaeforero@hotmail.com>
2025-04-07 15:24:26 -0500

    redTerminos_ajuste_texto

--- src/sections/redTerminos.js
+++ src/sections/redTerminos.js
@@ -5,13 +5,11 @@ import ResponsiveIframe from '../components/responseIframe';
 
 export default function RedTerminos() {
 
-    
-
     return (
         <>
         <Container className="margin_bottom_m">
              <h1 className="width_100 text_center margin_top_l">Red de Términos</h1>
-             <h5 className="width_100 text_center margin_bottom_m">Lorem ipsum dolor ... </h5>
+             <h5 className="width_100 text_center margin_bottom_m">Interactúe con las palabras clave del Tesauro a través de sus redes y relaciones temáticas.</h5>
              <ResponsiveIframe  src="https://relatoria.jep.gov.co/red/index.php" title="Red de términos clave" />
         </Container>  
         </>
