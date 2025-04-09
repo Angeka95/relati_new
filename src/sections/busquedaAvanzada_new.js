b98a0e3754a8ebd65522059421c1f042f1e5dc9a
claudiainbytes <claudiaeforero@hotmail.com>
2025-04-09 18:04:32 -0500

    busquedaAvanzada_refactor_temp

---
+++ src/sections/busquedaAvanzada_new.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+const BusquedaAvanzada = ( ) => {
+    
+  return (
+    <div>
+      Busqueda Avanzada
+    </div>
+  ); 
+};
+
+export default BusquedaAvanzada;
