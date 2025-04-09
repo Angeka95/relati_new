1488da2032c54bc31bc5de3d4bb7e9dd453a0f91
claudiainbytes <claudiaeforero@hotmail.com>
2025-04-09 12:08:11 -0500

    aplicacionService_fixing_catch_error_ERR_NETWORK

--- src/services/aplicacion.js
+++ src/services/aplicacion.js
@@ -13,16 +13,33 @@ const getStatusRelatiAppxMantenimiento = () => {
   const request =  axios.get('https://relatoria.jep.gov.co/getappstate', config);
   return request.then(response => { 
     if((response.data.status !== undefined) || (response.data.status === 401) || (response.data.status === 403)) {
-      return { "data": [], "status_info": { "status": response.data.status, "reason": response.data.reason }};
+      return { "data": [{ "enabled": true }], "status_info": { "status": response.data.status, "reason": response.data.reason }};
     } else {
-      return { "data": [{ 
-                    "enabled": response.data.enabled, 
-                }], 
-                "status_info": { "status": 200, "reason": "OK" } };
+      return { "data": [{ "enabled": response.data.enabled }], "status_info": { "status": 200, "reason": "OK" } };
     }
-  }).catch(error => { 
-    return { "data": [], "status_info": { "status": error.response.data.status, "reason": error.response.data.reason } };
+  })
+  .catch(error => { 
+    let error_response =  {} 
+    if(error.response !== undefined ){
+      error_response = { "data":[{ "enabled": true }], "status_info": { "status": error.response.data.status, "reason": error.response.data.reason } };
+    } else {
+      switch (error.code) {
+        case "ERR_NETWORK":
+          error_response = { "data":[{ "enabled": true }], "status_info": { "status": "ERR_NETWORK", "reason": error.message } };
+          break;
+        case "ECONNREFUSED":
+          error_response = { "data":[{ "enabled": true }], "status_info": { "status": "ECONNREFUSED", "reason": error.message } };
+          break;
+        case "ECONNRESET":
+          error_response = { "data":[{ "enabled": true }], "status_info": { "status": "ECONNRESET", "reason": error.message } };
+          break;
+        default:
+          error_response = { "data":[{ "enabled": true }], "status_info": { "status": "ERR_NETWORK", "reason": error.message } };
+      }
+    }
+    return error_response;
   });
+
 }
 
 export default { getStatusRelatiAppxMantenimiento }
