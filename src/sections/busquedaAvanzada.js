fa5dccfc7832e194ee80520e23813e9d40c30815
claudiaestupinan <claudiaeforero@hotmail.com>
2025-04-10 15:35:24 -0500

    busquedaAvanzada_creacionForm

--- src/sections/busquedaAvanzada.js
+++ src/sections/busquedaAvanzada.js
@@ -13,7 +13,7 @@ const BusquedaAvanzada = ( ) => {
             <Grid item xs={8} sm={8} md={12} lg={12} xl={12}>
               <Grid item xs={8} sm={8} md={8} lg={8} xl={8}>
                 <h1 className="text_center margin_top_l">Búsqueda avanzada</h1>
-                <h5 className="text_center margin_bottom_m">Delimite su búsqueda usando una o más palabras claves para los siguientes parámetros.
+                <h5 className="text_center margin_bottom_l">Delimite su búsqueda usando una o más palabras claves para los siguientes parámetros.
                   {/* <br></br>
                         Si va a  incluir más de un criterio por campo, sepárelo con una coma. 
                   */}
